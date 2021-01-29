6a75d5fd73b470a369b41f9fa55e5599c44d4758
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-29 13:52:53 -0700

    work on useFolder hook

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import AddFolderButton from "./AddFolderButton";
 import Navbar from "./Navbar";
+import { useFolder } from "../../hooks/useFolder";
 
 export default function Dashboard() {
+  const { folder } = useFolder();
+
   return (
     <>
       <Navbar />
       <Container fluid>
-        <AddFolderButton />
+        <AddFolderButton currentFolder={folder} />
       </Container>
     </>
   );
