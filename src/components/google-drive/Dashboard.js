9f5b5e732b330cc2147cf2f7ec319a77cdd79c5d
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-30 12:48:22 -0700

    work on useFolder hook

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import AddFolderButton from "./AddFolderButton";
-import Navbar from "./Navbar";
 import { useFolder } from "../../hooks/useFolder";
+import AddFolderButton from "./AddFolderButton";
+import Folder from "./Folder";
+import Navbar from "./Navbar";
 
 export default function Dashboard() {
-  const { folder } = useFolder();
+  const { folder, childFolders } = useFolder();
 
   return (
     <>
       <Navbar />
       <Container fluid>
         <AddFolderButton currentFolder={folder} />
+        {childFolders.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFolders.map((childFolder) => (
+              <div
+                key={folder.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
       </Container>
     </>
   );
