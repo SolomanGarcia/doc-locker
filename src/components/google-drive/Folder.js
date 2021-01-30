b10f65f3380cb17b25675ac31f509c764a386d2d
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-30 13:03:35 -0700

    add styling to Folder.js

--- src/components/google-drive/Folder.js
+++ src/components/google-drive/Folder.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
 
 export default function Folder({ folder }) {
-  return <div>{folder.name}</div>;
+  return (
+    <Button variant="outline-dark" className="text-truncate w-100" as={Link}>
+      <FontAwesomeIcon icon={faFolder} className="mr-2" />
+      {folder.name}
+    </Button>
+  );
 }
